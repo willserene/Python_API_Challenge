f6e80226fa80cc260dfca648443b07e93fe46de5
William Serene <willserene@gmail.com>
2021-09-28 20:09:12 -0600

    removed api keys

--- VacationPy/api_keys.py
+++ VacationPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "e75367a54c3f1159d90a9ddfd55755f2"
+weather_api_key = ""
 
 # Google API Key
-g_key = "AIzaSyC-d0WXa8NtC9KsNWsqMkEjgBZ03t98Zvs"
+g_key = ""
--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "e75367a54c3f1159d90a9ddfd55755f2"
+weather_api_key = ""
 
 # Google API Key
-g_key = "AIzaSyBl0SkF4zhNmYAQ8ewiUvl_yNMD6-n5_vI"
+g_key = ""
